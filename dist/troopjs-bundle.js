b80434593c00a76dedb43cac70b2cf1608d868f4
Mikael Karon <mikael@karon.se>
2012-07-10 17:58:44 +0800

    bumped troopjs-jquery
    snapshot

--- dist/troopjs-bundle.js
+++ dist/troopjs-bundle.js
@@ -2785,15 +2785,15 @@ define('troopjs-jquery/hashchange',[ "jquery" ], function HashchangeModule($) {
 	}
 
 	Frame.prototype = {
-		getElement : function getElement() {
+		getElement : function () {
 			return this.element;
 		},
 
-		getHash : function getHash() {
+		getHash : function () {
 			return this.element.contentWindow.frameHash;
 		},
 
-		update : function update(hash) {
+		update : function (hash) {
 			var self = this;
 			var document = self.element.contentWindow.document;
 
