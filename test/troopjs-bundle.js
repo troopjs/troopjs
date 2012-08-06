e5b3e68b31b200ec19cdd31c2362407ea2c4af28
Mikael Karon <mikael@karon.se>
2012-08-06 15:27:10 +0800

    add troopjs-utils

--- test/troopjs-bundle.js
+++ test/troopjs-bundle.js
@@ -3,6 +3,7 @@ require.config({
 	paths : {
 		"compose" : "lib/composejs/compose",
 		"troopjs-core" : "lib/troopjs-core/src",
+		"troopjs-utils" : "lib/troopjs-utils/src",
 		"troopjs-jquery" : "lib/troopjs-jquery/src",
 		"troopjs-requirejs" : "lib/troopjs-requirejs/src"
 	}
