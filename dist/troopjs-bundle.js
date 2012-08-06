9938e38b37703ebb84bcf3f4e8fc8623968a285c
Mikael Karon <mikael@karon.se>
2012-08-06 15:28:19 +0800

    bumped troopjs-utils
    updated gruntfile to include troopjs-utils/arg
    
    snapshot

--- dist/troopjs-bundle.js
+++ dist/troopjs-bundle.js
@@ -2135,6 +2135,119 @@ define('troopjs-utils/when',[ "jquery" ], function WhenModule($) {
 	return $.when;
 });
 /*!
+ * TroopJS Utils arg module
+ * @license TroopJS Copyright 2012, Mikael Karon <mikael@karon.se>
+ * Released under the MIT license.
+ */
+define('troopjs-utils/arg',[],function ArgModule() {
+	var PUSH = Array.prototype.push;
+	var SUBSTRING = String.prototype.substring;
+	var RE_BOOLEAN = /^(?:false|true)$/i;
+	var RE_BOOLEAN_TRUE = /^true$/i;
+	var RE_DIGIT = /^\d+$/;
+
+	return function arg() {
+		var self = this;
+		var result = [];
+		var length;
+		var from;
+		var to;
+		var i;
+		var c;
+		var a;
+		var q = false;
+
+		// Iterate over string
+		for (from = to = i = 0, length = self.length; i < length; i++) {
+			// Get char
+			c = self[i];
+
+			switch(c) {
+				case "\"" :
+				case "'" :
+					// If we are currently quoted...
+					if (q === c) {
+						// Stop quote
+						q = false;
+
+						// Store result (no need to convert, we know this is a string)
+						PUSH.call(result, SUBSTRING.call(self, from, to));
+					}
+					// Otherwise
+					else {
+						// Start quote
+						q = c;
+					}
+
+					// Update from/to
+					from = to = i + 1;
+					break;
+
+				case "," :
+					// Continue if we're quoted
+					if (q) {
+						to = i + 1;
+						break;
+					}
+
+					// If we captured something...
+					if (from !== to) {
+						a = SUBSTRING.call(self, from, to);
+
+						if (RE_BOOLEAN.test(a)) {
+							a = RE_BOOLEAN_TRUE.test(a);
+						}
+						else if (RE_DIGIT.test(a)) {
+							a = +a;
+						}
+
+						// Store result
+						PUSH.call(result, a);
+					}
+
+					// Update from/to
+					from = to = i + 1;
+					break;
+
+				case " " :
+				case "\t" :
+					// Continue if we're quoted
+					if (q) {
+						to = i + 1;
+						break;
+					}
+
+					// Update from/to
+					if (from === to) {
+						from = to = i + 1;
+					}
+					break;
+
+				default :
+					// Update to
+					to = i + 1;
+			}
+		}
+
+		// If we captured something...
+		if (from !== to) {
+			a = SUBSTRING.call(self, from, to);
+
+			if (RE_BOOLEAN.test(a)) {
+				a = RE_BOOLEAN_TRUE.test(a);
+			}
+			else if (RE_DIGIT.test(a)) {
+				a = +a;
+			}
+
+			// Store result
+			PUSH.call(result, a);
+		}
+
+		return result;
+	};
+});
+/*!
  * TroopJS jQuery action plug-in
  * @license TroopJS Copyright 2012, Mikael Karon <mikael@karon.se>
  * Released under the MIT license.
