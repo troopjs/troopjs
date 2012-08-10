7b80859c746ad3ead3f4f5e8ec98eac67642292b
Mikael Karon <mikael@karon.se>
2012-08-10 18:03:54 +0800

    bumped troopjs-core
    updated gruntfile
    
    snapshot

--- dist/troopjs-bundle.js
+++ dist/troopjs-bundle.js
@@ -2050,52 +2050,6 @@ define('troopjs-core/widget/application',[ "../component/widget", "troopjs-utils
 	});
 });
 /*!
- * TroopJS widget/sandbox component
- * @license TroopJS Copyright 2012, Mikael Karon <mikael@karon.se>
- * Released under the MIT license.
- */
-define('troopjs-core/widget/sandbox', [ "../component/widget", "jquery" ], function SandboxModule(Widget, $) {
-	var $ELEMENT = "$element";
-	var _$ELEMENT = "_" + $ELEMENT;
-
-	return Widget.extend({
-		"sig/initialize" : function onInitialize(signal, deferred) {
-			var self = this;
-
-			// Store ref to current $element
-			var $element = self[_$ELEMENT] = self[$ELEMENT];
-
-			// Get the contentWindow
-			var contentWindow = $element.get(0).contentWindow;
-
-			// Set $element to iframe document element
-			self[$ELEMENT] = $(contentWindow.ownerDocument || contentWindow.document);
-
-			if (deferred) {
-				deferred.resolve();
-			}
-		},
-
-		"sig/start" : function onStart(signal, deferred) {
-			this.weave(deferred);
-		},
-
-		"sig/stop" : function onStop(signal, deferred) {
-			this.unweave(deferred);
-		},
-
-		"sig/finalize": function onFinalize(signal, deferred){
-
-			delete this[_$ELEMENT];
-			
-			if (deferred){
-				deferred.resolve();
-			}
-		}
-	});
-});
-
-/*!
  * TroopJS Utils each component
  * @license TroopJS Copyright 2012, Mikael Karon <mikael@karon.se>
  * Released under the MIT license.
