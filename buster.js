694b6e7c9fb7334a26490fe82921522cfc95da52
Mikael Karon <mikael@karon.se>
2012-08-21 14:48:01 +0800

    update paths

--- buster.js
+++ buster.js
@@ -14,8 +14,7 @@ module.exports.tests = {
 	resources : [
 		"resources/requirejs/require.js",
 		"resources/config.requirejs.js",
-		"src/lib/composejs/compose.js",
-		"src/lib/troopjs-*/src/**/*.js"
+		"src/**/*.*"
 	],
 
 	tests : [
