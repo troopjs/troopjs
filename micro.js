fbe7826e302c94ff853cc5291419be0063821ad2
Mikael Karon <mikael@karon.se>
2013-05-10 18:05:04 +0800

    Submodule lib/troopjs-core c1295f7..3bb6b27:   > Added console logger, pubsub logger and logger service. Closes troopjs/troopjs-core#101
    Added logger components to micro bundle.

--- micro.js
+++ micro.js
@@ -1,5 +1,7 @@
 define([
-	"troopjs-core/component/service",
+	"troopjs-core/logger/console",
+	"troopjs-core/logger/pubsub",
+	"troopjs-core/logger/service",
 	"troopjs-browser/ajax/service",
 	"troopjs-browser/application/widget",
 	"troopjs-browser/route/widget",
