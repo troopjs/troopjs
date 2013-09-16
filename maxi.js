0d268e4caf9779d9744e0d368a3373cca0d88156
Mikael Karon <mikael@karon.se>
2013-09-16 17:54:35 +0800

    troopjs - 2.0.2-4+1a5a0af

--- maxi.js
+++ maxi.js
@@ -1,5 +1,5 @@
 /**
- * troopjs - 2.0.2-4+548a7b0 © Mikael Karon mailto:mikael@karon.se
+ * troopjs - 2.0.2-4+1a5a0af © Mikael Karon mailto:mikael@karon.se
  * @license MIT http://troopjs.mit-license.org/
  */
 
@@ -5316,5 +5316,5 @@ define('troopjs-browser/dimensions/widget',[ "../component/widget", "troopjs-jqu
 		}
 	});
 });
-define('troopjs/version',[],function () { return "2.0.2-4+548a7b0"; });
+define('troopjs/version',[],function () { return "2.0.2-4"; });
 define(['troopjs/version'], function (main) { return main; });
--- mini.js
+++ mini.js
@@ -1,5 +1,5 @@
 /**
- * troopjs - 2.0.2-4+548a7b0 © Mikael Karon mailto:mikael@karon.se
+ * troopjs - 2.0.2-4+1a5a0af © Mikael Karon mailto:mikael@karon.se
  * @license MIT http://troopjs.mit-license.org/
  */
 
@@ -4628,5 +4628,5 @@ define('troopjs-jquery/noconflict',[ "jquery" ], function ($) {
 	return $.noConflict(true);
 });
 
-define('troopjs/version',[],function () { return "2.0.2-4+548a7b0"; });
+define('troopjs/version',[],function () { return "2.0.2-4"; });
 define(['troopjs/version'], function (main) { return main; });
