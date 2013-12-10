40707c05c87f6ca68f966e06c7a904b541c61e17
Mikael Karon <mikael@karon.se>
2013-12-10 18:27:12 +0800

    Make `docs` part of the regular build.

--- Gruntfile.js
+++ Gruntfile.js
@@ -292,6 +292,6 @@ module.exports = function(grunt) {
 	grunt.registerTask("compile", [ "requirejs", "git-describe", "rewrite" ]);
 	grunt.registerTask("compress", [ "uglify" ]);
 	grunt.registerTask("test", [ "buster" ]);
-	grunt.registerTask("default", [ "compile", "compress", "usebanner" ]);
 	grunt.registerTask("docs", [ "jsduck" ]);
+	grunt.registerTask("default", [ "compile", "compress", "usebanner", "docs" ]);
 };
