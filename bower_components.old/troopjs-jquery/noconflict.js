387dbd0c9b9123132aecb6cacff6145ff0a269ee
Garry Yao <yaojun85@gmail.com>
2014-02-08 17:44:40 +0800

    troopjs - 2.0.2-8+c472868

--- bower_components.old/troopjs-jquery/noconflict.js
+++
@@ -1,9 +0,0 @@
-/**
-* TroopJS jquery/noconflict
-* @license MIT http://troopjs.mit-license.org/ © Tristan Guo mailto:tristanguo@outlook.com
-*/
-define([ "jquery" ], function ($) {
-	"use strict";
-
-	return $.noConflict(true);
-});
