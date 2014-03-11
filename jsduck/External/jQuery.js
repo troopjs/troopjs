981eae08a9352629395e82d5ff236eb9af17b52a
Mikael Karon <mikael@karon.se>
2014-03-11 14:56:47 +0800

    Moved external documentation of `jQuery` and `Promise` to `jsduck` subfolder.
    Copied documentation from when.js for `Promise`

---
+++ jsduck/External/jQuery.js
@@ -0,0 +1,25 @@
+/**
+ * jQuery
+ * @class $
+ * @alternateClassName jQuery
+ * @extends External
+ * @singleton
+ */
+
+/**
+ * jQuery events
+ * @class $.event
+ * @singleton
+ */
+
+/**
+ * jQuery expressions
+ * @class $.expr
+ * @singleton
+ */
+
+/**
+ * jQuery methods
+ * @class $.fn
+ * @singleton
+ */
