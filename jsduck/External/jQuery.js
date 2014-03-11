8ca3d0c3c36126a8b06f64f480f040edc2c0b788
Mikael Karon <mikael@karon.se>
2014-03-11 15:32:34 +0800

    Updated JSDoc for the `jQuery` object

--- jsduck/External/jQuery.js
+++ jsduck/External/jQuery.js
@@ -1,9 +1,17 @@
 /**
- * jQuery
+ * Represents the [jQuery](http://jquery.com) object
  * @class $
  * @alternateClassName jQuery
  * @extends External
  * @singleton
+ */
+
+/**
+ * Return a collection of matched elements either found in the DOM based on passed argument(s) or created by passing an HTML string.
+ * @method constructor
+ * @param {String} selector A string containing a selector expression
+ * @param {HTMLElement|jQuery} context A DOM Element, Document, or jQuery to use as context
+ * @returns {jQuery}
  */
 
 /**
