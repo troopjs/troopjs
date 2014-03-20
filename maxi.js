d0e1ad3917defc310e8d4d18e690c9daa1f42e14
Garry Yao <yaojun85@gmail.com>
2014-03-20 11:56:59 +0800

    Included jquery loom plugin in maxi.

--- maxi.js
+++ maxi.js
@@ -7,6 +7,7 @@ define([
 	"troopjs-data/cache/component",
 	"troopjs-data/cache/service",
 	"troopjs-data/query/service",
+	"troopjs-browser/loom/plugin",
 	"troopjs-jquery/noconflict",
 	"troopjs-requirejs/multiversion",
 	"troopjs-requirejs/shadow",
