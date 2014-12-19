ddb66d59b58835e8d78d67eda9813043f2bf3c36
Mikael Karon <mikael@karon.se>
2014-12-19 12:12:36 +0800

    Convert tests to AMD tests

--- require.js
+++ require.js
@@ -1,35 +1,11 @@
-/**
- * @license MIT http://troopjs.mit-license.org/
- */
-/*globals require:false*/
 require({
-	"baseUrl" : "bower_components",
-	"packages" : [ "jquery", "when", "poly", "mu-getargs", "mu-select", "mu-selector-set", "mu-merge", "mu-unique", "mu-jquery-destroy", "troopjs" ].map(function (name) {
-		var main;
-		var location;
-
-		switch (name) {
-			case "jquery":
-				location = "jquery/dist";
-				/* falls through */
-
-			case "when":
-				main = name;
-				break;
-
-			case "poly":
-				main = "es5";
-				break;
-
-			case "troopjs":
-				location = "..";
-				break;
-		}
-
-		return {
-			"name": name,
-			"location": location,
-			"main": main
-		}
-	})
+	"baseUrl": "bower_components",
+	"packages": [{
+		"name": "jquery",
+		"location": "jquery/dist",
+		"main": "jquery"
+	}, {
+		"name": "troopjs",
+		"location": ".."
+	}]
 });
